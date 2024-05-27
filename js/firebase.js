7eb08fb8e8cb0edc8055244305237257b4e38089
devika1910 <102358413+devika1910@users.noreply.github.com>
2024-05-27 20:23:49 +0530

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkZO9_PxSDKS2gxIVxXgYwEyiERSaDS7Q",
+  authDomain: "college-explorer-portal.firebaseapp.com",
+  projectId: "college-explorer-portal",
+  storageBucket: "college-explorer-portal.appspot.com",
+  messagingSenderId: "748456291775",
+  appId: "1:748456291775:web:1aaa015a4d2e56ee59450a",
+  measurementId: "G-PQE04LZY8F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
